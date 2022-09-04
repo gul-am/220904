9fd317661f61924f3d51b208dd6d11b6f92deb77
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-09-04 19:43:41 +0200

    chore: initial blades

---
+++ resources/views/pathway.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite("resources/css/app.css")
+</head>
+<body>
+
+    <div id="toast-top-left" class="flex absolute top-5 left-5 items-center p-4 space-x-4 w-full max-w-xs text-gray-500 bg-white rounded-lg divide-x divide-gray-200 shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+        <div class="text-sm font-normal">Top left positioning.</div>
+    </div>
+    <div id="toast-top-right" class="flex absolute top-5 right-5 items-center p-4 space-x-4 w-full max-w-xs text-gray-500 bg-white rounded-lg divide-x divide-gray-200 shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+        <div class="text-sm font-normal">Top right positioning.</div>
+    </div>
+    <div id="toast-bottom-right" class="flex absolute right-5 bottom-5 items-center p-4 space-x-4 w-full max-w-xs text-gray-500 bg-white rounded-lg divide-x divide-gray-200 shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+        <div class="text-sm font-normal">Bottom right positioning.</div>
+    </div>
+    <div id="toast-bottom-left" class="flex absolute bottom-5 left-5 items-center p-4 space-x-4 w-full max-w-xs text-gray-500 bg-white rounded-lg divide-x divide-gray-200 shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+        <div class="text-sm font-normal">Bottom left positioning.</div>
+    </div>
+
+
+</body>
+</html>
