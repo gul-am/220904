cae41ec23ee3faa41f15d84d3c14def1d2f8e114
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-09-15 21:46:49 +0200

    shore: landing blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,25 +19,10 @@
           <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
             <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
               <div class="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
-                <div class="flex w-full items-center justify-between md:w-auto">
-
-
-                </div>
+                <div class="flex w-full items-center justify-between md:w-auto"></div>
               </div>
-
             </nav>
           </div>
-
-          <!--
-            Mobile menu, show/hide based on menu open state.
-
-            Entering: "duration-150 ease-out"
-              From: "opacity-0 scale-95"
-              To: "opacity-100 scale-100"
-            Leaving: "duration-100 ease-in"
-              From: "opacity-100 scale-100"
-              To: "opacity-0 scale-95"
-          -->
 
         <main class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
           <div class="sm:text-center lg:text-left">
@@ -45,16 +30,18 @@
             <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                 <div class="w-full max-w-md space-y-8">
                   <div>
-                    <img class="mx-auto h-12 w-auto" src="" alt="Fish Trace-v3 Logo">
-                    <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Login</h2>
+                    <a href="{{ route('pathway') }}">
+                        <img class="mx-auto h-12 w-auto" src="" alt="Fish Trace-v3 Logo">
+                        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">&nbsp;</h2>
+                    </a>
 
                   </div>
                   <form class="mt-8 space-y-6" action="#" method="POST">
                     <input type="hidden" name="remember" value="true">
                     <div class="-space-y-px rounded-md shadow-sm">
                       <div>
-                        <label for="email-address" class="sr-only">Email address</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address">
+                        <label for="username" class="sr-only">Username</label>
+                        <input id="username" name="username" type="username" autocomplete="username" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="username">
                       </div>
                       <div>
                         <label for="password" class="sr-only">Password</label>
@@ -64,12 +51,11 @@
 
                     <div class="flex items-center justify-between">
                       <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+                        <label for="remember-me" class="ml-2 block text-sm text-gray-900"></label>
                       </div>
 
                       <div class="text-sm">
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"></a>
                       </div>
                     </div>
 
@@ -77,11 +63,11 @@
                       <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                           <!-- Heroicon name: mini/lock-closed -->
-                          <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
                           </svg>
                         </span>
-                        Sign in
+                        Login
                       </button>
                     </div>
                   </form>
@@ -95,7 +81,7 @@
       </div>
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
+      <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full" src="{{ asset('statics/images/vessel.jpg') }}" alt="">
     </div>
   </div>
 
